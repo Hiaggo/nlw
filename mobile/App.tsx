456a335e378abfc88906ee91a96bd741afc1e8ae
Hiaggo <42723368+Hiaggo@users.noreply.github.com>
2020-08-08 17:07:04 -0300

    Adição da Splash Screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { AppLoading } from 'expo';
+import AppIntroSlider  from 'react-native-app-intro-slider';
 
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
 
-import Landing from './src/pages/Landing';
 import AppStack from './src/routes/AppStack';
 
 export default function App() {
